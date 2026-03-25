7449437069af6f297275ca988d885a11053d2cf6
pavan <subrahmanyamkondeti773@gmail.com>
2026-03-25 22:08:52 +0530

    fixed static root issue

--- Detecting_and_Diagnosing_Brain_Tumor_MRI_Image/settings.py
+++ Detecting_and_Diagnosing_Brain_Tumor_MRI_Image/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +130,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
